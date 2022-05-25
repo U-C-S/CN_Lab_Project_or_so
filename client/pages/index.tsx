1efeef15333632bf981cda58d3cb31e188369ad9
U-C-S <ucs3721@outlook.com>
2022-05-25 10:50:22 +0530

    add login page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
-import { OverbookedUiShell } from "../components/Layout";
+import { AuthenticationForm } from "../components";
 
 export default function IndexPage() {
 	return (
-		<OverbookedUiShell>
-			<p>{process.env.NEXT_PUBLIC_API_SERVER}</p>
-		</OverbookedUiShell>
+		<AuthenticationForm />
 	);
 }
