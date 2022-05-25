d963d04b9088aabdb6f8071137f9437373453dfe
U-C-S <ucs3721@outlook.com>
2022-05-25 10:54:53 +0530

    Server code tweaks

--- api/app.js
+++ api/app.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import helmet from "helmet";
 import session from "express-session";
 
-import mainRoute from "./routes/index.js";
 import authRoute from "./routes/auth/auth.js";
 
 const app = express();
@@ -13,19 +12,9 @@ app.use(
 		origin: "*",
 	})
 );
-app.use(session({
-	secret: "superdupersecret",
-	resave: true,
-	saveUninitialized: true,
-	cookie: {
-		domain: "localhost",
-		maxAge: 1000 * 60 * 60 * 24 * 7,
-	},
-}));
 app.use(json());
 app.use(helmet());
 
 app.use("/auth", authRoute);
-app.use("/api", mainRoute);
 
 export default app;
